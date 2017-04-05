671c8398c2c256ca11669949a01d3ac74d582c80
Ryan Buxton <rbuxton1@gmail.com>
2017-04-05 09:39:06 -0600

    initial

---
+++ android/src/main/java/com/rbuxton/dingeoneer/main/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.rbuxton.dingeoneer.main.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rbuxton.dingeoneer.main.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        initialize(new Main(), configuration);
+    }
+}
