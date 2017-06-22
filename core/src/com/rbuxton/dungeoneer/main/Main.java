49c206854257d424ff63c69042314b24cad8d64d
Ryan Buxton <rbuxton1@gmail.com>
2017-06-21 22:29:28 -0600

    Removed Android and updated libGDX
    Wow a lot changed. No more android sub-directory, assets now under core. From now on some extra mokeying is required to get the game to go while in eclipse.

---
+++ core/src/com/rbuxton/dungeoneer/main/Main.java
@@ -0,0 +1,59 @@
+package com.rbuxton.dungeoneer.main;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.rbuxton.dungeoneer.map.WorldMap;
+import com.rbuxton.dungeoneer.misc.Constants;
+import com.rbuxton.dungeoneer.rooms.RoomGeneric;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends Game {
+    private SpriteBatch batch;
+    private Texture image;
+    private WorldMap wm;
+    private OrthographicCamera cam;
+    private Viewport port;
+
+    @Override
+    public void create() {
+    	cam = new OrthographicCamera();
+		port = new FillViewport(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, cam);
+		cam.setToOrtho(false);
+		port.apply();
+		cam.position.set(cam.viewportWidth/2.0f, cam.viewportHeight/2.0f, 0f);
+    	
+        batch = new SpriteBatch();
+        System.out.println(Gdx.files.isLocalStorageAvailable());
+        System.out.println(Gdx.files.getLocalStoragePath());
+        setScreen(new GameScreen(this)); //actual way we want to do this
+    }
+
+    @Override
+    public void render() {
+    	cam.update();
+        
+        batch.setProjectionMatrix(cam.combined);
+        super.render();
+        
+        if(Gdx.input.isKeyJustPressed(Keys.F5) || Gdx.input.justTouched()){
+        }
+    }
+
+    public OrthographicCamera getCam(){ return cam; }
+    public WorldMap getWorldMap(){ return wm; }
+    public SpriteBatch getBatch(){ return batch; }
+    
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
---
+++ desktop/src/com/rbuxton/dungeoneer/main/desktop/DesktopLauncher.java
@@ -0,0 +1,27 @@
+package com.rbuxton.dungeoneer.main.desktop;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.Graphics.DisplayMode;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rbuxton.dungeoneer.main.Main;
+
+/** Launches the desktop (LWJGL) application. */
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static LwjglApplication createApplication() {
+        return new LwjglApplication(new Main(), getDefaultConfiguration());
+    }
+
+    private static LwjglApplicationConfiguration getDefaultConfiguration() {
+        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+        configuration.title = "dungeoneer-mk3";
+        configuration.fullscreen = true;
+        configuration.width = 1920;
+        configuration.height = 1080;
+        return configuration;
+    }
+}
